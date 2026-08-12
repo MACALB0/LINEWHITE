3148f885904bf1d7d10b2ef376657682c94d7e27
Marcos Castillo <marcos.castillo@up.ac.pa>
2026-08-11 22:00:49 -0600

    Se mejora el login verificacion de sesion y redireccion a login

--- tests/unit/login.test.js
+++ tests/unit/login.test.js
@@ -1,40 +1,178 @@
-// login.test.js
 const { code_login } = require('../../controllers/components/login');
 const { code_vista_login } = require('../../controllers/components/vista_login');
 
 
 describe('Login Controller Tests', () => {
 
-    test('should redirect user to index page', async () => {
+    describe('code_login', () => {
 
-        const req = {};
+        test('should redirect user to index page when there is no valid session', async () => {
 
-        const res = {
-            redirect: jest.fn()
-        };
+            const req = {};
 
-        await code_login(req, res);
+            const res = {
+                redirect: jest.fn(),
+                render: jest.fn()
+            };
 
-        expect(res.redirect)
-            .toHaveBeenCalledWith('/index');
+            await code_login(req, res);
+
+            expect(res.redirect)
+                .toHaveBeenCalledWith('/');
+
+            expect(res.render)
+                .not.toHaveBeenCalled();
+
+        });
+
+
+        test('should render dashboard for user type 0', async () => {
+
+            const req = {
+                session: {
+                    usuario: {
+                        id_tipo_usuario: 0
+                    }
+                }
+            };
+
+            const res = {
+                redirect: jest.fn(),
+                render: jest.fn()
+            };
+
+            await code_login(req, res);
+
+            expect(res.render)
+                .toHaveBeenCalledWith(
+                    'secciones/vista_dashboard',
+                    {
+                        breadcrumb_name: 'Dashboard'
+                    }
+                );
+
+            expect(res.redirect)
+                .not.toHaveBeenCalled();
+
+        });
+
+
+        test('should render dashboard for user type 1', async () => {
+
+            const req = {
+                session: {
+                    usuario: {
+                        id_tipo_usuario: 1
+                    }
+                }
+            };
+
+            const res = {
+                redirect: jest.fn(),
+                render: jest.fn()
+            };
+
+            await code_login(req, res);
+
+            expect(res.render)
+                .toHaveBeenCalledWith(
+                    'secciones/vista_dashboard',
+                    {
+                        breadcrumb_name: 'Dashboard'
+                    }
+                );
+
+            expect(res.redirect)
+                .not.toHaveBeenCalled();
+
+        });
 
     });
 
 
-    test('should render login view correctly', async () => {
+    describe('code_vista_login', () => {
 
-        const req = {};
+        test('should render login view when there is no valid session', async () => {
 
-        const res = {
-            render: jest.fn()
-        };
+            const req = {};
 
-        await code_vista_login(req, res);
+            const res = {
+                redirect: jest.fn(),
+                render: jest.fn()
+            };
 
-        expect(res.render)
-            .toHaveBeenCalledWith('login/vista_login');
+            await code_vista_login(req, res);
+
+            expect(res.render)
+                .toHaveBeenCalledWith('login/vista_login');
+
+            expect(res.redirect)
+                .not.toHaveBeenCalled();
+
+        });
+
+
+        test('should render dashboard for user type 0', async () => {
+
+            const req = {
+                session: {
+                    usuario: {
+                        id_tipo_usuario: 0
+                    }
+                }
+            };
+
+            const res = {
+                redirect: jest.fn(),
+                render: jest.fn()
+            };
+
+            await code_vista_login(req, res);
+
+            expect(res.render)
+                .toHaveBeenCalledWith(
+                    'secciones/vista_dashboard',
+                    {
+                        breadcrumb_name: 'Dashboard'
+                    }
+                );
+
+            expect(res.redirect)
+                .not.toHaveBeenCalled();
+
+        });
+
+
+        test('should render dashboard for user type 1', async () => {
+
+            const req = {
+                session: {
+                    usuario: {
+                        id_tipo_usuario: 1
+                    }
+                }
+            };
+
+            const res = {
+                redirect: jest.fn(),
+                render: jest.fn()
+            };
+
+            await code_vista_login(req, res);
+
+            expect(res.render)
+                .toHaveBeenCalledWith(
+                    'secciones/vista_dashboard',
+                    {
+                        breadcrumb_name: 'Dashboard'
+                    }
+                );
+
+            expect(res.redirect)
+                .not.toHaveBeenCalled();
+
+        });
 
     });
-
 
 });
